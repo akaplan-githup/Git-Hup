c2c3b0f39b8399a8156522d58db4d74bd6141e51
ahmet <akaplan.githup@gmail.com>
2020-06-30 20:54:12 -0400

    Comment

---
+++ src/GeneralExamples/Examples_Palindromik.java
@@ -0,0 +1,40 @@
+package GeneralExamples;
+
+import java.util.Scanner;
+
+public class Examples_Palindromik {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Check Palindromik: ");
+        int number =scanner.nextInt();
+
+        if(number <10){rakam(number);}
+        else{
+
+
+            String x=String.valueOf(number);
+
+            String y="";
+
+            for (int i = x.length()-1; i >=0 ; i--) {
+
+
+                y= y+ x.charAt(i);
+            }
+
+            if(y.equals(x)){
+                System.out.println(x+"- Palindromik.");
+            }
+            else{
+                System.out.println(x+"- Non Palindromik.");
+            }
+
+        }
+
+
+    }
+    public static void rakam(int a){
+        System.out.println(a+"- Digist are always Palindromik!! ");
+    }
+}
